c6c6d7fa03d4288ea263c3a243a60a79077d97fe
vanvan <vns.28singh@gmail.com>
2020-01-05 23:15:50 -0500

    Quick, Merge, Bubble Working

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,19 +2,43 @@ from matplotlib import pyplot as plt
 from celluloid import Camera
 import numpy as np
 import sys
+import time
+import argparse
 
 sys.setrecursionlimit(1500)
 
 
 class Sort:
     def __init__(self, nums, len, cam):
-        print("In init of Sort")
         self.nums = nums
         self.len = len
         self.cam = cam
         self.ans = self.nums
         self.y_axis = np.arange(self.len)
         plt.bar(self.y_axis, self.nums, color='orange')
+        self.cam.snap()
+
+    def partition(self, arr, l, r):
+        i = l - 1
+        pivot = arr[r]
+        for j in range(l, r):
+            if arr[j] <= pivot:
+                i = i + 1
+                t = arr[i]
+                arr[i] = arr[j]
+                arr[j] = t
+        t = arr[i + 1]
+        arr[i + 1] = arr[r]
+        arr[r] = t
+        plt.bar(self.y_axis, arr, color='orange')
+        self.cam.snap()
+        return i + 1
+
+    def quick_sort(self, arr, l, r):
+        if l < r:
+            p = self.partition(arr, l, r)
+            self.quick_sort(arr, l, p - 1)
+            self.quick_sort(arr, p + 1, r)
 
     def merge(self, arr, l, m, r):
         n1 = m - l + 1
@@ -48,7 +72,6 @@ class Sort:
             k = k + 1
         plt.bar(self.y_axis, arr, color='orange')
         self.cam.snap()
-        print(arr)
 
     def merge_sort(self, arr, l, r):
         if l < r:
@@ -58,10 +81,8 @@ class Sort:
             self.merge(arr, l, m, r)
 
     def bubble_sort(self):
-        print("In bubble sort")
         y_axis = np.arange(self.len)
-        plt.bar(y_axis, self.nums, color='blue')
-        # plt.plot(self.nums)
+        plt.bar(y_axis, self.nums, color='orange')
         self.cam.snap()
         for i in range(0, self.len - 1, 1):
             for j in range(0, self.len - i - 1, 1):
@@ -69,32 +90,40 @@ class Sort:
                     temp = self.nums[j]
                     self.nums[j] = self.nums[j + 1]
                     self.nums[j + 1] = temp
-                    # plt.plot(self.nums)
-                    plt.bar(y_axis, self.nums, color='blue')
+                    plt.bar(y_axis, self.nums, color='orange')
                     self.cam.snap()
-        animation = self.cam.animate(interval=500, blit=True, repeat=False, repeat_delay=1000)
-        animation.save('bubble_sort_animation.gif', writer='imagemagick')
-        print(self.nums)
 
 
 def main():
-    print("Enter length: ")
-    len = int(input())
+    parser = argparse.ArgumentParser(description='Train Dependency Parsing Model')
+    parser.add_argument('--len', type=int, default=5, help='Array size')
+    parser.add_argument('--algo', type=str, help='Sorting Algorithm')
+    args = parser.parse_args()
+    len = args.len
+    alg = args.algo
     nums = []
     for i in range(len, 0, -1):
         nums.append(i)
     fig = plt.figure()
     cam = Camera(fig)
     obj1 = Sort(nums, len, cam)
-    # obj1.bubble_sort()
-    print(nums)
-    # y_axis = np.arange(len)
-    # plt.bar(y_axis, nums, color='orange')
-    cam.snap()
-    obj1.merge_sort(nums, 0, len - 1)
-    print(nums)
+    if alg == "bubble":
+        start_time = time.time()
+        obj1.bubble_sort()
+        elapsed_time = time.time() - start_time
+
+    elif alg == "merge":
+        start_time = time.time()
+        obj1.merge_sort(nums, 0, len - 1)
+        elapsed_time = time.time() - start_time
+
+    elif alg == "quick":
+        start_time = time.time()
+        obj1.quick_sort(nums, 0, len - 1)
+        elapsed_time = time.time() - start_time
+    print("Time : ", elapsed_time)
     animation = cam.animate(interval=800, blit=True, repeat=False, repeat_delay=1000)
-    animation.save('merge_sort_animation.gif', writer='imagemagick')
+    animation.save(alg + '_sort_animation.gif', writer='imagemagick')
 
 
 if __name__ == '__main__':
